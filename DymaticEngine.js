0de4a7dde3db518969388a8500c3c5e83852692a
Dymatic Engine <79347543+DymaticEngine@users.noreply.github.com>
2021-02-20 14:48:56 +1100

    Add files via upload

---
+++ DymaticEngine.js
@@ -0,0 +1,104 @@
+// JavaScript Document
+
+// Returns true if the specified element has been scrolled into the viewport.
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('FadeInAnimation') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+    var $elem = $('.FadeInAnimationEarly, .FadeInAnimationNormal, .FadeInAnimationLate');
+
+    // If the animation has already been started
+    if ($elem.hasClass('start')) return;
+
+    if (isElementInViewport($elem)) {
+        // Start the animation
+        $elem.addClass('start');
+    }
+}
+
+// Capture scroll events
+$(window).scroll(function(){
+    checkAnimation();
+});
+
+// Dynamic Logo Color
+'use strict';
+
+function setupIcons() {
+  const lightSchemeIcon = document.querySelector('link#light-scheme-icon');
+  const darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
+
+  function setLight() {
+    document.head.append(lightSchemeIcon);
+    darkSchemeIcon.remove();
+  }
+
+  function setDark() {
+    lightSchemeIcon.remove();
+    document.head.append(darkSchemeIcon);
+  }
+
+
+  const matcher = window.matchMedia('(prefers-color-scheme:dark)');
+  function onUpdate() {
+    if (matcher.matches) {
+      setDark();
+    } else {
+      setLight();
+    }
+  }
+  matcher.addListener(onUpdate);
+  onUpdate();
+}
+
+setupIcons();
+
+//Cookie Read and Write
+
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var user=getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+     user = prompt("Please enter your name:","");
+     if (user != "" && user != null) {
+       setCookie("username", user, 30);
+     }
+  }
+}
